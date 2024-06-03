8bc2c86a29d88a46c749683737dab2c383e92da5
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-06-03 20:01:00 -0300

    refactor(lombok): implemented lombok to model

--- apibank/src/main/java/com/api/bank/entity/TransferModel.java
+++ apibank/src/main/java/com/api/bank/entity/TransferModel.java
@@ -1,14 +1,19 @@
 package com.api.bank.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
 @Table(name = "tb_transfer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransferModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -21,47 +26,4 @@ public class TransferModel {
     @Column(name = "value_transfer")
     private BigDecimal valueTransfer;
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public BankUserModel getUserReceiver() {
-        return userReceiver;
-    }
-
-    public void setUserReceiver(BankUserModel userReceiver) {
-        this.userReceiver = userReceiver;
-    }
-
-    public BankUserModel getUserSender() {
-        return userSender;
-    }
-
-    public void setUserSender(BankUserModel userSender) {
-        this.userSender = userSender;
-    }
-
-    public BigDecimal getValueTransfer() {
-        return valueTransfer;
-    }
-
-    public void setValueTransfer(BigDecimal valueTransfer) {
-        this.valueTransfer = valueTransfer;
-    }
-
-    public TransferModel() {}
-
-    public TransferModel(UUID id,
-                         BankUserModel userReceiver,
-                         BankUserModel userSender,
-                         BigDecimal valueTransfer) {
-        this.id = id;
-        this.userReceiver = userReceiver;
-        this.userSender = userSender;
-        this.valueTransfer = valueTransfer;
-    }
 }
