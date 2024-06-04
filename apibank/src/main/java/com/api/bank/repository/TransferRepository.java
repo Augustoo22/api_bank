1b08b9887430b4072517da3b5833e9a0498a070f
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-06-03 21:55:57 -0300

    refactor(transfer repository): added @repository

--- apibank/src/main/java/com/api/bank/repository/TransferRepository.java
+++ apibank/src/main/java/com/api/bank/repository/TransferRepository.java
@@ -2,8 +2,8 @@ package com.api.bank.repository;
 
 import com.api.bank.entity.TransferModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
-public interface TransferRepository extends JpaRepository<TransferModel, UUID> {
-}
+@Repository
+public interface TransferRepository extends JpaRepository<TransferModel, UUID> {}
