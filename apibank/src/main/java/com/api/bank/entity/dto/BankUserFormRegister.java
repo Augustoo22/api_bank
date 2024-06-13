a9b056582b65f85b4d0b430a53497fcbfcade850
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-06-13 13:15:16 -0300

    refactor(register): updated register to be certain with all fields of bank user

---
+++ apibank/src/main/java/com/api/bank/entity/dto/BankUserFormRegister.java
@@ -0,0 +1,27 @@
+package com.api.bank.entity.dto;
+
+import com.api.bank.entity.BankUserModel;
+import com.api.bank.entity.enums.EnumUserType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+public record BankUserFormRegister(@NotBlank String username,
+                                   @NotBlank int userAge,
+                                   @NotBlank String userEmail,
+                                   @NotBlank String userPassword,
+                                   @NotBlank BigDecimal userBalance,
+                                   @NotNull String userCPF,
+                                   @NotNull EnumUserType userType) {
+
+    // Implemented DTO to instantiate an User object
+    public static BankUserModel toUserRequest(BankUserFormRegister user) {
+        return new BankUserModel( user.username,
+                                  user.userAge,
+                                  user.userEmail,
+                                  user.userPassword,
+                                  user.userBalance,
+                                  user.userCPF,
+                                  user.userType);
+    }
+}
