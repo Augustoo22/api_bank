902d724b30daf85b0b12b54c6ea89752e95da1a1
GuilhermeMLeal <guilherme.mleal.11@gmail.com>
2024-06-14 11:02:56 -0300

    feat: implemented id User in methods

--- apibank/src/main/java/com/api/bank/entity/dto/TransferForm.java
+++ apibank/src/main/java/com/api/bank/entity/dto/TransferForm.java
@@ -4,10 +4,11 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 public record TransferForm(
         @DecimalMin("0.01") @NotNull BigDecimal valueTransfer,
-        @NotNull Long payer,
-        @NotNull Long payee
+        @NotNull UUID payer,
+        @NotNull UUID payee
         ) {
 }
