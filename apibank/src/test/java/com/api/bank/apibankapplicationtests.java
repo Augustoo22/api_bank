4f1ea45ff6a852905e292030396b76a85fbef944
49297818846 <victoraugustodocarmo32@gmail.com>
2024-06-14 19:36:39 -0300

    fix: codigo corrigido pois ele nao rodava

--- apibank/src/test/java/com/api/bank/apibankapplicationtests.java
+++ apibank/src/test/java/com/api/bank/apibankapplicationtests.java
@@ -1,15 +1,18 @@
 package com.api.bank;
 
-import com.api.bank.repository.BankRepository;
+import com.api.bank.repository.BankUserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
 
 @WebMvcTest
 class apibankapplicationtests {
 
-	@MockBean
-	BankRepository bankRepository;
+	@Autowired
+	private BankUserRepository bankUserRepository;
+
 
 	@Test
 	void contextLoads() { }
