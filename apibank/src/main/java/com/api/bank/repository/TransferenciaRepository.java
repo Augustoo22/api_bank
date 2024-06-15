06b265f492e9710fee7c60f1aa88e2d8a8c440e4
Victor Carmo <victoraugustodocarmo32@gmail.com>
2024-06-15 06:23:37 -0300

    feat: repositorio do transferencia
    Esse repositorio tem o JpaRepository que por conta disso e possivel realizar o metodo CRUD para manipular com o banco de dados

--- apibank/src/main/java/com/api/bank/repository/TransferenciaRepository.java
+++ apibank/src/main/java/com/api/bank/repository/TransferenciaRepository.java
@@ -1,2 +1,8 @@
-package com.api.bank.repository;public class TransferenciaRepository {
+package com.api.bank.repository;
+import com.api.bank.model.Transferencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
 }
