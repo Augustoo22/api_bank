58ca6739dd49db839a2c8a96a36fcb5be21549aa
Victor Carmo <victoraugustodocarmo32@gmail.com>
2024-06-15 06:22:44 -0300

    feat: repositorio do usuario
    Esse repositorio tem o JpaRepository que por conta disso e possivel realizar o metodo CRUD para manipular com o banco de dados

--- apibank/src/main/java/com/api/bank/repository/UsuarioRepository.java
+++ apibank/src/main/java/com/api/bank/repository/UsuarioRepository.java
@@ -1,2 +1,11 @@
-package com.api.bank.repository;public class UsuarioRepository {
+package com.api.bank.repository;
+
+import com.api.bank.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
+
