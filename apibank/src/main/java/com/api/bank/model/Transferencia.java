a678419f4e638bb877b2d33200ad925d45a90e39
Victor Carmo <victoraugustodocarmo32@gmail.com>
2024-06-15 07:13:04 -0300

    feat: model transferencia

--- apibank/src/main/java/com/api/bank/model/Transferencia.java
+++ apibank/src/main/java/com/api/bank/model/Transferencia.java
@@ -1,2 +1,45 @@
-package com.api.bank.model;public class Transferencia {
+package com.api.bank.model;
+import jakarta.persistence.*;
+
+@Entity
+public class Transferencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String origem;
+    private String destino;
+    private double valor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
 }
