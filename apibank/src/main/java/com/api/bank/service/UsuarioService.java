827647b78e366e752bfaacd6d9e9e39fa2e5865b
Victor Carmo <victoraugustodocarmo32@gmail.com>
2024-06-15 06:27:01 -0300

    feat: usuario service
    service e responsavel por implementar os metodos para manipular o usuario e consegue acessar o 'UsuarioRepository'

--- apibank/src/main/java/com/api/bank/service/UsuarioService.java
+++ apibank/src/main/java/com/api/bank/service/UsuarioService.java
@@ -1,2 +1,29 @@
-package com.api.bank.service;public class UsuarioService {
+package com.api.bank.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.api.bank.model.Usuario;
+import com.api.bank.repository.UsuarioRepository;
+import java.util.List;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> listarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    public void deletar(Long id) {
+        usuarioRepository.deleteById(id);
+    }
 }
