79de5e7c0bde5e101db83ae298bd6d7f1a30822f
GuilhermeMLeal <guilherme.mleal.11@gmail.com>
2024-06-16 14:49:47 -0300

    feat: implemented controller to comunicate with user service

--- apibank/src/main/java/com/api/bank/controller/UsuarioController.java
+++ apibank/src/main/java/com/api/bank/controller/UsuarioController.java
@@ -1,109 +1,118 @@
-    package com.api.bank.controller;
-    
-    import com.api.bank.model.Usuario;
-    import com.api.bank.service.UsuarioService;
-    import com.api.bank.util.JwtUtil;
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.http.HttpHeaders;
-    import org.springframework.http.HttpStatus;
-    import org.springframework.http.ResponseEntity;
-    import org.springframework.security.authentication.AuthenticationManager;
-    import org.springframework.security.core.userdetails.UserDetails;
-    import org.springframework.stereotype.Controller;
-    import org.springframework.ui.Model;
-    import org.springframework.web.bind.annotation.*;
-    import org.springframework.web.servlet.ModelAndView;
+package com.api.bank.controller;
 
-    @Controller
-    @RequestMapping("/usuarios")
-    public class UsuarioController {
+import com.api.bank.model.Usuario;
+import com.api.bank.service.UsuarioService;
+import com.api.bank.util.JwtUtil;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-        @Autowired
-        private UsuarioService usuarioService;
+import java.util.Optional;
 
-        @Autowired
-        private AuthenticationManager authenticationManager;
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
 
-        @Autowired
-        private JwtUtil jwtUtil;
+    @Autowired
+    private UsuarioService usuarioService;
 
-        @GetMapping("")
-        public String login() {
-            return "paginaLogin";
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("")
+    public String login(Model model, @RequestParam(value = "error", required = false) String error) {
+        if (error != null) {
+            model.addAttribute("erro", "Usuário ou senha inválidos");
         }
-
-        @GetMapping("/cadastro")
-        public String cadastroForm(Model model) {
-            model.addAttribute("usuario", new Usuario());
-            return "cadastroUser";
-        }
-
-        @PostMapping("/salvar")
-        public String salvar(@ModelAttribute("usuario") Usuario usuario) {
-            usuarioService.salvar(usuario);
-            return "redirect:/usuarios";
-        }
-
-        @PostMapping("")
-        public String createAuthenticationToken(@RequestParam String email, @RequestParam String senha, Model model) {
-            Usuario usuario = usuarioService.buscarPorEmailESenha(email, senha);
-            if (usuario == null) {
-                model.addAttribute("erro", "Usuário ou senha inválidos");
-                return "paginaLogin";
-            }
-
-            UserDetails userDetails = usuarioService.loadUserByUsername(email);
-            String jwt = jwtUtil.generateToken(userDetails);
-
-            model.addAttribute("jwt", jwt);
-            return "redirect:/usuarios/pagina-inicial/" + usuario.getId();
-        }
-
-
-
-        @GetMapping("/pagina-inicial/{id}")
-        public String paginaInicial(@PathVariable("id") Long id, Model model) {
-            Usuario cadastro = usuarioService.buscarPorId(id);
-            model.addAttribute("bank", cadastro);
-            return "pagina_inicial";
-        }
-
-
-        @GetMapping("/registro/{id}")
-        public ModelAndView paginaRegistro(@PathVariable("id") Long id, Model model) {
-            Usuario cadastro = usuarioService.buscarPorId(id);
-            ModelAndView modelAndView = new ModelAndView("registro.html");
-            modelAndView.addObject("bank", cadastro);
-            return modelAndView;
-        }
-
-        @GetMapping("/pix/{id}")
-        public ModelAndView paginaPix(@PathVariable("id") Long id, Model model) {
-            Usuario cadastro = usuarioService.buscarPorId(id);
-            ModelAndView modelAndView = new ModelAndView("pix.html");
-            modelAndView.addObject("bank", cadastro);
-            return modelAndView;
-        }
-
-        @GetMapping("/editar/{id}")
-        public String editarUsuario(@PathVariable("id") Long id, Model model) {
-            Usuario usuario = usuarioService.buscarPorId(id);
-            model.addAttribute("usuario", usuario);
-            return "editar.html";
-        }
-
-        @PostMapping("/atualizar")
-        public ResponseEntity<String> atualizarUsuario(@ModelAttribute("usuario") Usuario usuario, Model model) {
-            try {
-                usuarioService.salvar(usuario);
-                HttpHeaders headers = new HttpHeaders();
-                headers.add("Location", "/usuarios/pagina-inicial/" + usuario.getId());
-                return new ResponseEntity<>(headers, HttpStatus.FOUND);
-            } catch (Exception e) {
-                model.addAttribute("erro", "Erro ao atualizar usuário: " + e.getMessage());
-                return new ResponseEntity<>("Erro ao atualizar usuário: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-
+        return "paginaLogin";
     }
+
+    @PostMapping("")
+    public String fazerLogin(@RequestParam String email, @RequestParam String senha, HttpSession session, Model model) {
+        Optional<Usuario> usuarioOptional = usuarioService.buscarPorEmail(email);
+        System.out.println(usuarioOptional);
+        if (usuarioOptional.isEmpty() || !passwordEncoder.matches(senha, usuarioOptional.get().getSenha())) {
+            model.addAttribute("erro", "Usuário ou senha inválidos");
+            return "redirect:/usuarios?error";
+        }
+
+        Usuario usuario = usuarioOptional.get();
+        session.setAttribute("usuarioId", usuario.getId());
+
+        UserDetails userDetails = usuarioService.loadUserByUsername(email);
+        String token = jwtUtil.generateToken(userDetails);
+
+        return "redirect:/usuarios/pagina-inicial/" + usuario.getId() + "?token=" + token;
+    }
+
+    @GetMapping("/pagina-inicial/{id}")
+    public String paginaInicial(@PathVariable("id") Long id, @RequestParam("token") String token, Model model) {
+        Optional<Usuario> cadastro = usuarioService.buscarPorId(id);
+        if (cadastro.isEmpty()) {
+            return "redirect:/usuarios?error";
+        }
+        model.addAttribute("bank", cadastro.get());
+        model.addAttribute("token", token);
+        return "pagina_inicial";
+    }
+
+    @GetMapping("/cadastro")
+    public String cadastroForm(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "cadastroUser";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(@ModelAttribute("usuario") Usuario usuario) {
+        usuarioService.salvar(usuario);
+        return "redirect:/usuarios";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editarUsuario(@PathVariable("id") Long id, Model model) {
+        Optional<Usuario> usuario = usuarioService.buscarPorId(id);
+        if (usuario.isEmpty()) {
+            return "redirect:/usuarios?error";
+        }
+        model.addAttribute("usuario", usuario.get());
+        return "editar";
+    }
+
+    @PostMapping("/atualizar")
+    public String atualizarUsuario(@ModelAttribute("usuario") Usuario usuario, Model model) {
+        try {
+            usuarioService.salvar(usuario);
+            return "redirect:/usuarios/pagina-inicial/" + usuario.getId();
+        } catch (Exception e) {
+            model.addAttribute("erro", "Erro ao atualizar usuário: " + e.getMessage());
+            return "editar";
+        }
+    }
+
+    @GetMapping("/registro/{id}")
+    public String paginaRegistro(@PathVariable("id") Long id, Model model) {
+        Optional<Usuario> cadastro = usuarioService.buscarPorId(id);
+        if (cadastro.isEmpty()) {
+            return "redirect:/usuarios?error";
+        }
+        model.addAttribute("bank", cadastro.get());
+        return "registro";
+    }
+
+    @GetMapping("/pix/{id}")
+    public String paginaPix(@PathVariable("id") Long id, Model model) {
+        Optional<Usuario> cadastro = usuarioService.buscarPorId(id);
+        if (cadastro.isEmpty()) {
+            return "redirect:/usuarios?error";
+        }
+        model.addAttribute("bank", cadastro.get());
+        return "pix";
+    }
+}
