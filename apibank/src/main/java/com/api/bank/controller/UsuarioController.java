f844476098a3f16db5fbce28e8a9fae08f58d3fe
GuilhermeMLeal <guilherme.mleal.11@gmail.com>
2024-06-16 14:57:28 -0300

    refactor: user controller added all functions to user login

--- apibank/src/main/java/com/api/bank/controller/UsuarioController.java
+++ apibank/src/main/java/com/api/bank/controller/UsuarioController.java
@@ -1,118 +1,134 @@
-package com.api.bank.controller;
+    package com.api.bank.controller;
+    
+    import com.api.bank.model.Usuario;
+    import com.api.bank.service.UsuarioService;
+    import com.api.bank.util.JwtUtil;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.http.HttpHeaders;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.security.authentication.AuthenticationManager;
+    import org.springframework.security.core.userdetails.UserDetails;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.*;
+    import org.springframework.web.servlet.ModelAndView;
 
-import com.api.bank.model.Usuario;
-import com.api.bank.service.UsuarioService;
-import com.api.bank.util.JwtUtil;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+    import java.util.Optional;
 
-import java.util.Optional;
+    @Controller
+    @RequestMapping("/usuarios")
+    // Aqui está incicando que a classe é um controlador do Spring
+    // Indica que todas URL's ira começar com /usuarios
 
-@Controller
-@RequestMapping("/usuarios")
-public class UsuarioController {
+    public class UsuarioController {
 
-    @Autowired
-    private UsuarioService usuarioService;
+        @Autowired
+        private UsuarioService usuarioService;
 
-    @Autowired
-    private JwtUtil jwtUtil;
+        @Autowired
+        private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+        @Autowired
+        private JwtUtil jwtUtil;
+        //Os tres @Autowired são para o Spring injetar instâncias nessa classe controller
 
-    @GetMapping("")
-    public String login(Model model, @RequestParam(value = "error", required = false) String error) {
-        if (error != null) {
-            model.addAttribute("erro", "Usuário ou senha inválidos");
+        @GetMapping("")
+        public String login() {
+            return "paginaLogin";
         }
-        return "paginaLogin";
-    }
+        //Realiza o mapeamento de requisição GET, para acessar /usuarios
+        // No 'return' ele está retornando a a pagina de login
 
-    @PostMapping("")
-    public String fazerLogin(@RequestParam String email, @RequestParam String senha, HttpSession session, Model model) {
-        Optional<Usuario> usuarioOptional = usuarioService.buscarPorEmail(email);
-        System.out.println(usuarioOptional);
-        if (usuarioOptional.isEmpty() || !passwordEncoder.matches(senha, usuarioOptional.get().getSenha())) {
-            model.addAttribute("erro", "Usuário ou senha inválidos");
-            return "redirect:/usuarios?error";
+        @GetMapping("/cadastro")
+        public String cadastroForm(Model model) {
+            model.addAttribute("usuario", new Usuario());
+            return "cadastroUser";
         }
+        //Mapeia outra requisicão GET para renderizar outra pagina só que dessa vez de cadastro
+        // para acessar /usuarios/cadastro
+        // Está retornando a tela de cadastro
 
-        Usuario usuario = usuarioOptional.get();
-        session.setAttribute("usuarioId", usuario.getId());
-
-        UserDetails userDetails = usuarioService.loadUserByUsername(email);
-        String token = jwtUtil.generateToken(userDetails);
-
-        return "redirect:/usuarios/pagina-inicial/" + usuario.getId() + "?token=" + token;
-    }
-
-    @GetMapping("/pagina-inicial/{id}")
-    public String paginaInicial(@PathVariable("id") Long id, @RequestParam("token") String token, Model model) {
-        Optional<Usuario> cadastro = usuarioService.buscarPorId(id);
-        if (cadastro.isEmpty()) {
-            return "redirect:/usuarios?error";
-        }
-        model.addAttribute("bank", cadastro.get());
-        model.addAttribute("token", token);
-        return "pagina_inicial";
-    }
-
-    @GetMapping("/cadastro")
-    public String cadastroForm(Model model) {
-        model.addAttribute("usuario", new Usuario());
-        return "cadastroUser";
-    }
-
-    @PostMapping("/salvar")
-    public String salvar(@ModelAttribute("usuario") Usuario usuario) {
-        usuarioService.salvar(usuario);
-        return "redirect:/usuarios";
-    }
-
-    @GetMapping("/editar/{id}")
-    public String editarUsuario(@PathVariable("id") Long id, Model model) {
-        Optional<Usuario> usuario = usuarioService.buscarPorId(id);
-        if (usuario.isEmpty()) {
-            return "redirect:/usuarios?error";
-        }
-        model.addAttribute("usuario", usuario.get());
-        return "editar";
-    }
-
-    @PostMapping("/atualizar")
-    public String atualizarUsuario(@ModelAttribute("usuario") Usuario usuario, Model model) {
-        try {
+        @PostMapping("/salvar")
+        public String salvar(@ModelAttribute("usuario") Usuario usuario) {
             usuarioService.salvar(usuario);
+            return "redirect:/usuarios";
+        }
+        //Mapeia a requesição POST para que seja possivel salvar os dados a requisição dele
+        //e feita através do botão salvar
+        //que está na pagina 'cadastroUser', ele salva o usuario que é recebido pelo formulario
+        //através do serviço 'UsuarioService' e redireciona para pagina login
+
+        @PostMapping("")
+        public String createAuthenticationToken(@RequestParam String email, @RequestParam String senha, Model model) {
+            boolean usuario = usuarioService.validarLogin(email, senha);
+            if (usuario == false) {
+                model.addAttribute("erro", "Usuário ou senha inválidos");
+                return "paginaLogin";
+            }
+
+            UserDetails userDetails = usuarioService.loadUserByUsername(email);
+            String jwt = jwtUtil.generateToken(userDetails);
+
+            model.addAttribute("jwt", jwt);
             return "redirect:/usuarios/pagina-inicial/" + usuario.getId();
-        } catch (Exception e) {
-            model.addAttribute("erro", "Erro ao atualizar usuário: " + e.getMessage());
-            return "editar";
         }
-    }
+        //Mapeia requisição POST, ele está buscando o usuário pelo email e senha usando o 'usuarioService'
+        //caso não exista ele retorna uma mensagem de erro, mas se o usuario é encontrado carrega as informações
+        //dele e gera p token
 
-    @GetMapping("/registro/{id}")
-    public String paginaRegistro(@PathVariable("id") Long id, Model model) {
-        Optional<Usuario> cadastro = usuarioService.buscarPorId(id);
-        if (cadastro.isEmpty()) {
-            return "redirect:/usuarios?error";
+        @GetMapping("/pagina-inicial/{id}")
+        public String paginaInicial(@PathVariable("id") Long id, Model model) {
+            Optional<Usuario> cadastro = usuarioService.buscarPorId(id);
+            model.addAttribute("bank", cadastro);
+            return "pagina_inicial";
         }
-        model.addAttribute("bank", cadastro.get());
-        return "registro";
-    }
+        //Mapeia o GET para '/usuarios/pagina-inicial/{id} o id é do usuario para acessar a pagina da api apos ser logado
+        //e tambem retorna a pagina inicial
 
-    @GetMapping("/pix/{id}")
-    public String paginaPix(@PathVariable("id") Long id, Model model) {
-        Optional<Usuario> cadastro = usuarioService.buscarPorId(id);
-        if (cadastro.isEmpty()) {
-            return "redirect:/usuarios?error";
+        @GetMapping("/registro/{id}")
+        public ModelAndView paginaRegistro(@PathVariable("id") Long id, Model model) {
+            Optional<Usuario> cadastro = usuarioService.buscarPorId(id);
+            ModelAndView modelAndView = new ModelAndView("registro.html");
+            modelAndView.addObject("bank", cadastro);
+            return modelAndView;
         }
-        model.addAttribute("bank", cadastro.get());
-        return "pix";
+        //Mapeia o GET para '/usuarios/registro/{id} o id é do usuario para conseguir fazer a navegação do usuario
+        //Retorna a pagina 'registro'
+
+        @GetMapping("/pix/{id}")
+        public ModelAndView paginaPix(@PathVariable("id") Long id, Model model) {
+            Optional<Usuario> cadastro = usuarioService.buscarPorId(id);
+            ModelAndView modelAndView = new ModelAndView("pix.html");
+            modelAndView.addObject("bank", cadastro);
+            return modelAndView;
+        }
+        //Mapeia o GET para '/usuarios/pix/{id} o id é do usuario para conseguir fazer a navegação do usuario e nessa
+        //pagina onde será possivel ele realizar a transferencia do saldo para outro usuario
+
+        @GetMapping("/editar/{id}")
+        public String editarUsuario(@PathVariable("id") Long id, Model model) {
+            Optional<Usuario> usuario = usuarioService.buscarPorId(id);
+            model.addAttribute("usuario", usuario);
+            return "editar.html";
+        }
+        // Aqui ele é direcionado para /usuarios/editar/{id} onde ele terá acesso as informações dele e podera editar
+        // retorna a pagina 'editar.html'
+
+        @PostMapping("/atualizar")
+        public ResponseEntity<String> atualizarUsuario(@ModelAttribute("usuario") Usuario usuario, Model model) {
+            try {
+                usuarioService.salvar(usuario);
+                HttpHeaders headers = new HttpHeaders();
+                headers.add("Location", "/usuarios/pagina-inicial/" + usuario.getId());
+                return new ResponseEntity<>(headers, HttpStatus.FOUND);
+            } catch (Exception e) {
+                model.addAttribute("erro", "Erro ao atualizar usuário: " + e.getMessage());
+                return new ResponseEntity<>("Erro ao atualizar usuário: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        //Mapeia um POST que está no formulario de edição para que é acionado após o botão salvar for apertado
+        //pegando as informações do formulario e se não tiver erros ele salva no usuarioService e redireciona para
+        //pagina inicial mas caso de erro retorna uma mensagem de erro.
+
     }
-}
