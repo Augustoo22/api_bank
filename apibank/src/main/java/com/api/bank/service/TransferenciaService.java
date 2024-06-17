349c29a911be25ba4ce73ffea3ac0cc4121abea9
GuilhermeMLeal <guilherme.mleal.11@gmail.com>
2024-06-17 11:14:09 -0300

    refactor(list for register): list of pix in specific account

--- apibank/src/main/java/com/api/bank/service/TransferenciaService.java
+++ apibank/src/main/java/com/api/bank/service/TransferenciaService.java
@@ -2,26 +2,28 @@ package com.api.bank.service;
 
 import com.api.bank.model.Transferencia;
 import com.api.bank.model.Usuario;
+import com.api.bank.model.dto.TransferenciaDTO;
 import com.api.bank.repository.TransferenciaRepository;
 import com.api.bank.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TransferenciaService {
 
     @Autowired
-    private TransferenciaRepository transferRepository;
+    private TransferenciaRepository transferenciaRepository;
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     @Transactional
     public String transferirPix(Transferencia transferencia) {
-        // Verifica se o usuário pagador existe e tem saldo suficiente
         Usuario pagador = usuarioRepository.findById(Long.parseLong(transferencia.getOrigem()))
                 .orElseThrow(() -> new IllegalArgumentException("Usuário pagador não encontrado com ID: " + transferencia.getOrigem()));
         double saldoPagador = pagador.getSaldo();
@@ -30,25 +32,24 @@ public class TransferenciaService {
             throw new IllegalArgumentException("Saldo insuficiente para realizar a transferência");
         }
 
-        // Busca o usuário beneficiário
         Usuario beneficiario = usuarioRepository.findById(Long.parseLong(transferencia.getDestino()))
                 .orElseThrow(() -> new IllegalArgumentException("Usuário beneficiário não encontrado com ID: " + transferencia.getDestino()));
 
-        // Realiza a transferência
         pagador.debit(valorTransferencia);
         beneficiario.credit(valorTransferencia);
 
-        // Salva a transferência no banco de dados
-        transferRepository.save(transferencia);
+        transferencia.setDataHora(LocalDateTime.now());
+        transferenciaRepository.save(transferencia);
 
         return "Transferência PIX realizada com sucesso: Valor: " + valorTransferencia +
                 ", da conta de ID: " + pagador.getId() + " para a conta de ID: " + beneficiario.getId();
     }
 
     @Transactional
-    public List<Transferencia> transferirPix(Long id) {
-        // Implemente a lógica para buscar as transações PIX do usuário com o id fornecido
-        return transferRepository.findAllByOrigemOrDestino(id);
+    public List<TransferenciaDTO> getTransferenciasByUser(String userId) {
+        List<Transferencia> transferenciasOrigem = transferenciaRepository.findAllByOrigem(userId);
+        List<Transferencia> transferenciasDestino = transferenciaRepository.findAllByDestino(userId);
+        transferenciasOrigem.addAll(transferenciasDestino);
+        return transferenciasOrigem.stream().map(TransferenciaDTO::new).collect(Collectors.toList());
     }
-
 }
